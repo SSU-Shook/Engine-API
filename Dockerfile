3b919d49fa21d6a2af380b340c614dbafff9d4d7
realsung <stealien_rnd@stealienrnd.local>
2024-05-27 23:04:49 +0900

    fix/port mismatch

--- Dockerfile
+++ Dockerfile
@@ -10,7 +10,7 @@ COPY requirements.txt /app
 
 RUN pip install --no-cache-dir -r /app/requirements.txt
 
-EXPOSE 8000
+EXPOSE 5000
 
 # CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
 CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -98,4 +98,4 @@ def read_files(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=5000)
